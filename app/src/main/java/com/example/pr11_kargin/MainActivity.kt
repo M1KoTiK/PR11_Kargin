efd8aba1d68c11dadd0efb2214a7ab9daffc2d00
KoTiK <temhbz@mail.ru>
2022-06-04 09:40:24 +0700

    screen2 add

--- app/src/main/java/com/example/pr11_kargin/MainActivity.kt
+++ app/src/main/java/com/example/pr11_kargin/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.pr11_kargin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun onClickSwitchActivity(view: View){
+        val intent = Intent(this, Screen2::class.java)
+        startActivity(intent)
+    }
+
+
+
 }
