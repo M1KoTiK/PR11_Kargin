b2c265d1a03887f14f904be4ba20315b62b8b6fa
KoTiK <temhbz@mail.ru>
2022-06-05 01:02:22 +0700

    3 экрана и всё почти как надо

--- app/src/main/java/com/example/pr11_kargin/Screen2.kt
+++ app/src/main/java/com/example/pr11_kargin/Screen2.kt
@@ -1,11 +1,17 @@
 package com.example.pr11_kargin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Screen2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_screen2)
+    }
+    fun onClickOpenScreen3(view: View){
+        val intent = Intent(this, screen3::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/pr11_kargin/screen3.kt
@@ -0,0 +1,11 @@
+package com.example.pr11_kargin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class screen3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screen3)
+    }
+}
